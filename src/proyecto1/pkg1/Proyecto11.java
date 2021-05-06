ffd59c9f8450d216409999c926251fc20911c71c
DESKTOP-LOHN354\manto <adony.torres29@gmail.com>
2021-05-06 11:59:12 -0600

    Creacion del menu CRUD

--- src/proyecto1/pkg1/Proyecto11.java
+++ src/proyecto1/pkg1/Proyecto11.java
@@ -5,6 +5,9 @@
  */
 package proyecto1.pkg1;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 /**
  *
  * @author Antonio
@@ -14,10 +17,31 @@ public class Proyecto11 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
         // TODO code application logic here
         //las pruebas de commit se eliminaron
-       
+        throws SQLException {
+        desplegarMenu();
     }
-    
+
+    private static void desplegarMenu() {
+        Scanner sc = new Scanner(System.in);
+        String menu;
+        
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+        System.out.println("|                       OPCIONES                          |");
+        System.out.println("|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-|");
+        System.out.println("|                                                         |");
+        System.out.println("|                 1. Crear  registros                     |");
+        System.out.println("|                 2. Consultar registros                  |");
+        System.out.println("|                 3. Actualizar registros                 |");
+        System.out.println("|                 4. Eliminar registros                   |");
+        System.out.println("|                 5. Salir                                |");
+        System.out.println("|                                                         |");
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+        System.out.println("Seleccionar opcion: ");
+        menu = sc.next();
+        
+    }
+
 }
