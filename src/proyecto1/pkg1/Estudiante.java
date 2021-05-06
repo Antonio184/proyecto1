111e65d4cb1dc9b75288e029fd1d437e6518fe08
Adony M <65377350+jamt29@users.noreply.github.com>
2021-05-06 12:36:26 -0600

    Update Estudiante.java

--- src/proyecto1/pkg1/Estudiante.java
+++ src/proyecto1/pkg1/Estudiante.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto1.pkg1;
 
-/**
- *
- * @author manto
- */
+
 public class Estudiante {
     
     private final String servidor = "jdbc:mysql://127.0.0.2:3306/bd_estudiante";
