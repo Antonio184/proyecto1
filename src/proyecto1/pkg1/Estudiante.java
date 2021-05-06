a745af131742bf4c69419e7484a0f329cc21fa7c
Antonio <Antonio@LAPTOP-KA2REOO1>
2021-05-06 12:33:14 -0600

    Variables

--- src/proyecto1/pkg1/Estudiante.java
+++ src/proyecto1/pkg1/Estudiante.java
@@ -11,4 +11,9 @@ package proyecto1.pkg1;
  */
 public class Estudiante {
     
+    private final String servidor = "jdbc:mysql://127.0.0.2:3306/bd_estudiante";
+    private final String usuario = "admin";
+    private final String clave = "admin";
+    private final String driver = "com.mysql.jbdc.Driver";
+    private static Connection conexion;
 }
