748a75d5e8eb431b78e70ac3c2dbf09eddd562c5
carmen30-Isa <82846294+carmen30-Isa@users.noreply.github.com>
2021-05-09 15:45:20 -0600

    Agregacion del diseño del CRUD en main.java

--- src/Estudiante/Main.java
+++ src/Estudiante/Main.java
@@ -4,10 +4,28 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) 
+        throws SQLException {
+        desplegarMenu(); 
         
     }
-
+        public static void desplegarMenu() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        String menu;
+        
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+        System.out.println("|                       OPCIONES                          |");
+        System.out.println("|-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-|");
+        System.out.println("|                                                         |");
+        System.out.println("|                 1. Crear  registros                     |");
+        System.out.println("|                 2. Consultar registros                  |");
+        System.out.println("|                 3. Actualizar registros                 |");
+        System.out.println("|                 4. Eliminar registros                   |");
+        System.out.println("|                 5. Salir                                |");
+        System.out.println("|                                                         |");
+        System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+        System.out.print("Seleccionar opcion: ");
+        menu = sc.next();
    
 }
-
+    }
