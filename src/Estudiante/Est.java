3e289ed6d902e91963373d587914e319895c2259
Larin <Larin@DESKTOP-V3B0KJR>
2021-05-09 19:35:07 -0600

    Finalizacion de la clase Est.java

--- src/Estudiante/Est.java
+++ src/Estudiante/Est.java
@@ -3,5 +3,49 @@ package Estudiante;
 
 
 public class Est {
-    
+        private int idEstudiante;
+    private String nomEstudiante;
+    private String apeEstudiante;
+    private String carnetEstudiante;
+    private int edadEstudiante;
+
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
+
+    public void setIdEstudiante(int idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+
+    public String getNomEstudiante() {
+        return nomEstudiante;
+    }
+
+    public void setNomEstudiante(String nomEstudiante) {
+        this.nomEstudiante = nomEstudiante;
+    }
+
+    public String getApeEstudiante() {
+        return apeEstudiante;
+    }
+
+    public void setApeEstudiante(String apeEstudiante) {
+        this.apeEstudiante = apeEstudiante;
+    }
+
+    public String getCarnetEstudiante() {
+        return carnetEstudiante;
+    }
+
+    public void setCarnetEstudiante(String carnetEstudiante) {
+        this.carnetEstudiante = carnetEstudiante;
+    }
+
+    public int getEdadEstudiante() {
+        return edadEstudiante;
+    }
+
+    public void setEdadEstudiante(int edadEstudiante) {
+        this.edadEstudiante = edadEstudiante;
+    }
 }
